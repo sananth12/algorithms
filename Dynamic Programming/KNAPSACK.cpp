edcbb6525affa9be4e5af8cf3242c21a9c657dd8
sananth12 <sananthanatarajan12@gmail.com>
2014-06-04 00:38:01 +0530

    DP

---
+++ Dynamic Programming/KNAPSACK.cpp
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+int main()
+{
+   int s,n;
+   cin>>s>>n;
+   int dp[100][100]={0};
+   int i,item[n][2];  // weight,value
+   for(i=1;i<=n;i++)
+       cin>>item[i][0]>>item[i][1];
+   for(i=1;i<=n;i++)
+        dp[i][0]=0;
+   for(i=1;i<=n;i++)
+   {
+       for(int j=0;j<=s;j++)
+       {
+           if(item[i][0]<=j)
+            dp[i][j]=max( dp[i-1][j], dp[i-1][j-item[i][0]]+ item[i][1] );
+            else
+            dp[i][j]=dp[i-1][j];
+
+          //  cout<<dp[i][j]<<" ";
+       }
+      // cout<<endl;
+   }
+    cout<<dp[n-1][s];
+return 0;
+}
---
+++ Dynamic Programming/README.md
@@ -0,0 +1,4 @@
+Dynamic Programming
+===================
+
+* Knapsack Problem
